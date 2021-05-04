8dd08b24c8471d7831c52df9299751fb20c5950b
rileymintable <rileyrountree@my.unt.edu>
2021-05-04 23:13:58 +0000

    My lil quote

--- get-quote.py
+++ get-quote.py
@@ -5,9 +5,11 @@ def primary():
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-  last = 13
+  last = 14
   rnd = random.randint(0, last)
+  diff = random.randint(0, last)
   print(quotes[rnd])
+  print(quotes[diff])
 
 if __name__== "__main__":
   primary()
